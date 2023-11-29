e9b2d91f6b6e0c2298fa211bd99aaba9c3935ccb
Dheeraj <73672766+DheerajYarlagadda@users.noreply.github.com>
2023-11-29 02:25:22 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,2 @@
-print("Dheeraj")
+print({'npp.8.4.2.Installer.x64.exe': {'path': 'https://jfrog.hub.vwgroup.com/artifactory/vwos-tools-generic-playground/test_Dheeraj/Notepad++/8.4.2/win/x64/npp.8.4.2.Installer.x64.exe', 'type': 'file', 'size': 4518024, 'created': '2023-11-27 14:12:13', 'modified': '2023-11-27 14:12:09', 'sha1': '19c1d8c029a6c34147ba38d9b455940ec7e625ab', 'md5': '235f59b6e878c512e8a6bd3dba4a2f1a'}}
+)
