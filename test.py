c9f4159fedd2c26dba7d3254155ba908ec4af76e
Dheeraj <73672766+DheerajYarlagadda@users.noreply.github.com>
2023-11-29 05:56:55 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-
+import json
 
 parser = ArgumentParser()
 parser.add_argument('-u', '--username', action='store', type=str, required=True)
@@ -9,5 +9,6 @@ parser.add_argument('-rj', '--recipe_json', action='store', type=str, required=T
 parser.add_argument('-dr', '--dryrun', action='store', type=str, default='False', required=False)
 args = parser.parse_args()
 
-print({'args': {'username': args.username, 'token': args.token, 'base_url': args.artifactory_url, 'recipe_json': args.recipe_json, 'dryrun': args.dryrun}}
-)
+x = {'args': {'username': args.username, 'token': args.token, 'base_url': args.artifactory_url, 'recipe_json': args.recipe_json, 'dryrun': args.dryrun}}
+json_output = json.dumps(x)
+print(json_output)
